2afea685699ea1c2c460351e40561ed1af251cff
Kyaw Aung Win <kyawagwin@gmail.com>
2020-08-25 10:01:44 +0800

    tested to regenerate test function

--- src/functions/testFunction.js
+++ src/functions/testFunction.js
@@ -1,15 +1,11 @@
 'use strict';
 
 module.exports.testFunction = async (event, context) => {
-  const response = {
+  return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
     }),
   };
-
-  return response;
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
